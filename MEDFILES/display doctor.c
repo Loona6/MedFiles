79543fd9e7ed174bd2bf1e48e79b852035682436
Rahatut <rahatuttahrim@iut-dhaka.edu>
2024-06-03 22:14:48 +0600

    eee
    eee

--- MEDFILES/display doctor.c
+++ MEDFILES/display doctor.c
@@ -10,32 +10,3 @@
 #define MAX_ID_LENGTH 20
 #define FILENAME "doctors.csv"
 
-struct Doctor {
-    char first_name[50];
-    char last_name[50];
-    char user_id[MAX_ID_LENGTH];
-};
-
-void display_doctors() {
-    FILE *file = fopen(FILENAME, "r");
-    if (file == NULL) {
-        printf("Error opening file.\n");
-        return;
-    }
-
-    struct Doctor doctor;
-    int count = 0;
-
-    while (fscanf(file, "%[^,],%[^,],%s\n", doctor.first_name, doctor.last_name, doctor.user_id) != EOF) {
-        count++;
-        printf("%d. %s %s (User ID: %s)\n", count, doctor.first_name, doctor.last_name, doctor.user_id);
-    }
-
-    fclose(file);
-
-    if (count == 0) {
-        printf("No doctors found.\n");
-    }
-
-
-}
--- MEDFILES/login page.c
+++ MEDFILES/login page.c
@@ -4,38 +4,3 @@
 #define MAX_USERNAME_LENGTH 20
 #define MAX_PASSWORD_LENGTH 20
 
-   void login(){
-    char username[MAX_USERNAME_LENGTH];
-    char password[MAX_PASSWORD_LENGTH];
-    char correct_username[] = "user";
-    char correct_password[] = "pass";
-
-    printf("\t\t\t\t\t\tLOGIN to enter MEDFILES\n");
-
-    // Prompt user for username
-    printf("Enter username: ");
-    fgets(username, MAX_USERNAME_LENGTH, stdin);
-    // Remove newline character from username if present
-    username[strcspn(username, "\n")] = 0;
-
-    // Prompt user for password
-    printf("Enter password: ");
-    fgets(password, MAX_PASSWORD_LENGTH, stdin);
-    // Remove newline character from password if present
-    password[strcspn(password, "\n")] = 0;
-
-    // Check if the entered username and password match
-    if (strcmp(username, correct_username) == 0 && strcmp(password, correct_password) == 0)
-    {
-        printf("Login successful!\n");
-        system("cls");
-    }
-    else{
-        printf("wrong password, Please enter the correct one");
-        system("cls");
-        printf("\n");
-        printf("Your password/username was wrong\n");
-        login();
-    }
-
-   }
