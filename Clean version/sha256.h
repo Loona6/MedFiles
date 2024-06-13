c974847d3a85beb2a9b5b268c941b817be59cc1b
Loona6 <163868229+Loona6@users.noreply.github.com>
2024-06-13 21:11:40 +0600

    last upload I swear

---
+++ Clean version/sha256.h
@@ -0,0 +1,20 @@
+#ifndef SHA256_H
+#define SHA256_H
+
+#include <stdint.h>
+
+#define SHA256_BLOCK_SIZE 32
+
+typedef struct
+{
+    uint8_t data[64];
+    uint32_t datalen;
+    uint64_t bitlen;
+    uint32_t state[8];
+} SHA256_CTX;
+
+void sha256_init(SHA256_CTX *ctx);
+void sha256_update(SHA256_CTX *ctx, const uint8_t data[], size_t len);
+void sha256_final(SHA256_CTX *ctx, uint8_t hash[]);
+
+#endif /* SHA256_H */
